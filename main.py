c2829231dacdcb8dbce72c9ef87a27a411e573ce
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-02-01 12:57:05 +0100

    return meg változtatva

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #megoldas
 def eredmeny( jPontja: list[int] , gPontja: list[int] ):
     if pOsszeg(jPontja) > 21:
-        return "Játékos nyert"
+        return "Játékos vesztett"
     elif pOsszeg(gPontja) > 21:
-        return "Játékos vesztet"
+        return "Játékos vesztett"
 
 def pOsszeg(lista: list[int]):
     i = 0
