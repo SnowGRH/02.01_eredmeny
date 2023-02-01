67383ff9e4aa3fffbe6fb35386ddc59d885d0ca0
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-02-01 12:14:07 +0100

    nagyobb mint 21 nyert

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #megoldas
-def eredmeny():
-    pass
+def eredmeny(jatokosPontja,gepPontja):
+    if jatokosPontja > 21:
+        return "Játékos nyert"
+    elif gepPontja > 21:
+        return "Játékos vesztet"
 #tesztek
