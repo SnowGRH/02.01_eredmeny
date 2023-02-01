942be57ec95ffe62fde3e1c694075cb206afc5d1
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-02-01 12:31:12 +0100

    pont szamolas

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #megoldas
 def eredmeny(jatokosPontja,gepPontja):
-    if jatokosPontja > 21:
+    if pOsszeg(jatokosPontja) > 21:
         return "Játékos nyert"
-    elif gepPontja > 21:
+    elif pOsszeg(gepPontja) > 21:
         return "Játékos vesztet"
+
+def pOsszeg(lista):
+    i = 0
+    pontosszeg = 0
+    while i < len(lista):
+        pontosszeg += lista[i]
+        i+=1
+    return pontosszeg
 #tesztek
